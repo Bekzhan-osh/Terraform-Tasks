b95ca14d9db4fc861dcdd43e66957fe5eae037a6
Bek <dtk1920@gmail.com>
2020-11-13 19:47:20 +0000

    r1soft

---
+++ task5/r1soft/r1soft.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "centos" {
+  ami                    = "${data.aws_ami.centos.id}"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.class2.id}"]
+  key_name               = "${aws_key_pair.class.key_name}"
+
+  tags = {
+    Name = "r1soft"
+  }
+}
+
+output "PIP" {
+  value = "${aws_instance.centos.public_ip}"
+}
